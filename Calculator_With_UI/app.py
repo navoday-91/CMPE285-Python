466bd16ed00f93bfe298c7995b0527a772e6e558
Navoday Tomar <navoday.91@gmail.com>
2018-11-02 16:29:24 -0700

    update assignment 2

--- Calculator_With_UI/app.py
+++ Calculator_With_UI/app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask import render_template
 from flask import request
-from datetime import datetime
+from datetime import datetime, timedelta
 import pytz
 import requests
 
@@ -67,20 +67,29 @@ def post_assignment2():
         return render_template('assignment2.html', error_data="Error in API Call")
     elif "Note" in stock_time_series:
         return render_template('assignment2.html', api_call_exceeded="No. Of API Calls Exceeded for minute/day")
-    if len(stock) < 2:
-        url = "https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=" + stock[0] + "&apikey=1VS6DHVM3DBWAYOH"
-        r = requests.get(url)
-        stock_meta = r.json()
-        stock.append(stock_meta['bestMatches'][0]["2. name"])
-    stock_data = {'name': stock[1], 'symbol': stock[0]}
+
+    url = "https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=" + stock[0] + "&apikey=1VS6DHVM3DBWAYOH"
+    r = requests.get(url)
+    stock_meta = r.json()
+    stock.append(stock_meta['bestMatches'][0]["2. name"])
+    stock.append(stock_meta['bestMatches'][0]["8. currency"])
+    stock_data = {'name': stock[1], 'symbol': stock[0], 'currency': stock[2]}
     today = stock_time_series['Meta Data']['3. Last Refreshed'].split(" ")[0]
+    last_day = datetime.strptime(today, '%Y-%m-%d')
+    last_day -= timedelta(days=1)
+    last_day = last_day.strftime('%Y-%m-%d')
     stock_data_today = stock_time_series['Time Series (Daily)'][today]
     stock_data['current'] = stock_data_today['4. close']
-    stock_data['change_by_vol'] = round(float(stock_data_today['4. close']) - float(stock_data_today['1. open']),2)
-    stock_data['change_by_percent'] = round((stock_data['change_by_vol']/float(stock_data_today['1. open']))*100,2)
+    stock_data['change_by_vol'] = round(float(stock_data_today['4. close']) -
+                                        float(stock_time_series['Time Series (Daily)'][last_day]['4. close']),2)
+    stock_data['change_by_percent'] = round((stock_data['change_by_vol']/
+                                        float(stock_time_series['Time Series (Daily)'][last_day]['4. close']))*100,2)
     if stock_data['change_by_vol'] >= 0:
         stock_data['change_by_vol'] = "+" + str(stock_data['change_by_vol'])
         stock_data['change_by_percent'] = "+" + str(stock_data['change_by_percent'])
+    else:
+        stock_data['change_by_vol'] = str(stock_data['change_by_vol'])
+        stock_data['change_by_percent'] = str(stock_data['change_by_percent'])
     pacific = datetime.now(pytz.timezone('US/Pacific'))
     temp = (pacific.strftime('%a-%b-%d-%H-%M-%S-%Y').split("-"))
     stock_data['time'] = " ".join(temp[:3])
